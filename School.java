14ad83ca689ec35c43545982e01012f748f89ae5
SheillyR <sheillyrolo@gmail.com>
2021-05-11 17:30:26 -0500

    Crear clase School

---
+++ School.java
@@ -0,0 +1,15 @@
+package school.management.system;
+
+/**
+ * Many teachers, many students.
+ * Implements teachers and students
+ * using an ArrayList.
+ */
+public class School {
+
+    private Teacher teacher;
+    private Student student;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+
+}
