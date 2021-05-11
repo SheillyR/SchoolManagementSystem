50011e38e68278a20b55a75a1f1ae63f97796fca
SheillyR <sheillyrolo@gmail.com>
2021-05-11 18:57:08 -0500

    anadir todos los getter y setter en la clase School

--- School.java
+++ School.java
@@ -26,5 +26,35 @@ public class School {
         totalMoneySpent = 0;
     }
 
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
 
+    public void setTeachers(List<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public void setTotalMoneyEarned(int totalMoneyEarned) {
+        this.totalMoneyEarned = totalMoneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    public void setTotalMoneySpent(int totalMoneySpent) {
+        this.totalMoneySpent = totalMoneySpent;
+    }
 }
