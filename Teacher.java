c30aae0e6e42beb7d98665ce026bdea1cab7b62f
SheillyR <sheillyrolo@gmail.com>
2021-05-11 15:42:18 -0500

    Crear y anadir propiedades a la clase Teacher

---
+++ Teacher.java
@@ -0,0 +1,12 @@
+package school.management.system;
+
+/**
+ * This class is responsible for keeping the track
+ * of teacher's name, id, salary.
+ */
+public class Teacher {
+
+    private int id;
+    private String name;
+    private int salary;
+}
