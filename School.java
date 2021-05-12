abc0f4559e3e8a3cb6f80c91bc167f3c9ef13d61
SheillyR <sheillyrolo@gmail.com>
2021-05-11 19:11:34 -0500

    modificacion de metodos y adicion de comentarios en la clase School

--- School.java
+++ School.java
@@ -16,6 +16,7 @@ public class School {
 
     /**
      * new school object is created
+     *
      * @param teachers list of teachers in the school.
      * @param students list of the students in the school.
      */
@@ -26,35 +27,69 @@ public class School {
         totalMoneySpent = 0;
     }
 
+    /**
+     * @return the list of teachers in the school.
+     */
     public List<Teacher> getTeachers() {
         return teachers;
     }
 
-    public void setTeachers(List<Teacher> teachers) {
-        this.teachers = teachers;
+    /**
+     * Adds a teacher to the school.
+     *
+     * @param teacher the teacher to be added.
+     */
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
     }
 
+    /**
+     * @return the list of students in the school.
+     */
     public List<Student> getStudents() {
         return students;
     }
 
-    public void setStudents(List<Student> students) {
-        this.students = students;
+    /**
+     * Adds a student to the school
+     *
+     * @param student the student to be added.
+     */
+
+    public void addStudent(Student student) {
+        students.add(student);
     }
+
+    /**
+     * @return the total money earned by school.
+     */
 
     public int getTotalMoneyEarned() {
         return totalMoneyEarned;
     }
 
-    public void setTotalMoneyEarned(int totalMoneyEarned) {
-        this.totalMoneyEarned = totalMoneyEarned;
+    /**
+     * Adds the total money earned by the school.
+     *
+     * @param MoneyEarned money that is supposed to be added.
+     */
+    public void updateTotalMoneyEarned(int MoneyEarned) {
+        totalMoneyEarned += MoneyEarned;
     }
 
+    /**
+     * @return the total money spent by the school.
+     */
     public int getTotalMoneySpent() {
         return totalMoneySpent;
     }
 
-    public void setTotalMoneySpent(int totalMoneySpent) {
-        this.totalMoneySpent = totalMoneySpent;
+    /**
+     * Update the money is spent by the school which
+     * is the salary given by the school to its teachers
+     * @param MoneySpent the money spent by school.
+     */
+    public void updateTotalMoneySpent(int MoneySpent) {
+        totalMoneyEarned -= MoneySpent;
     }
 }
