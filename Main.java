170efdc24d21a82de0fce32018eae002a1ccb27c
SheillyR <sheillyrolo@gmail.com>
2021-05-11 19:29:28 -0500

    Crear students en la clase Main

--- Main.java
+++ Main.java
@@ -6,6 +6,11 @@ public class Main {
         Teacher melissa = new Teacher(2,"Melissa",700);
         Teacher vanderhorn = new Teacher(3,"Vanderhorn",600);
 
+        Student tamasha = new Student(1,"Tamasha",4);
+        Student rakshith = new Student(2,"Rakshit",12);
+        Student rabbi = new Student(3,"Rabbi",5);
+
+
 
 
     }
