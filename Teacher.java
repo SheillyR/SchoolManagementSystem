d8355fccf47c250f9ada86555fcb54faae0d4722
SheillyR <sheillyrolo@gmail.com>
2021-05-11 20:14:41 -0500

    cambiar metodo en receiveSalary

--- Teacher.java
+++ Teacher.java
@@ -63,6 +63,6 @@ public class Teacher {
      */
     public void receiveSalary(int salary) {
         salaryEarned += salary;
-        School.updateTotalMoneyEarned(salary);
+        School.updateTotalMoneySpent(salary);
     }
 }
